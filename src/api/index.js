c5bbc8af6a900f9beddeb45f690a685458401a8b
hjha <hjhaaa.github@gmail.com>
2022-11-08 19:20:50 +0900

    feat: 회원 가입 api 요청 후 결과 출력

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-function registerUser(userData) {
+const registerUser = userData => {
 	const url = `http://localhost:3000/signup`;
 	return axios.post(url, userData);
-}
+};
 
 export { registerUser };
