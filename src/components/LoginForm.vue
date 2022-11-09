84f74bee7a412c3b990d372d638a244901236141
hjha <hjhaaa.github@gmail.com>
2022-11-09 19:53:44 +0900

    feat: 로그인 시 error 핸들링을 위해 try catch 구문 추가

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -26,14 +26,23 @@ export default {
 	},
 	methods: {
 		async submitForm() {
-			const userData = {
-				username: this.username,
-				password: this.password,
-			};
-			const { data } = await loginUser(userData);
-			console.log(data);
-            this.logMessage = `${data.user.username} 님 환영합니다.`;
+            try {
+                const userData = {
+                    username: this.username,
+                    password: this.password,
+                };
+                const { data } = await loginUser(userData);
+                this.logMessage = `${data.user.username} 님 환영합니다.`;
+            } catch (error) {
+                this.logMessage = error.response.data;
+            } finally {
+                this.initForm();
+            }
 		},
+        initForm() {
+            this.username = '';
+            this.password = '';
+        }
 	},
 };
 </script>
