76d08b286f9b71cd5fbc8a111ca027a61a5c505f
hjha <hjhaaa.github@gmail.com>
2022-11-10 21:26:26 +0900

    feat: 게시물 조회 화면 로딩스피너 추가 및 관련 코드 정리

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -1,12 +1,10 @@
 <template>
     <div>
-        <ul>
-            <li>
-                <div class="post-title"> {{ postItem.title}} </div>
-                <div class="post-contents"> {{ postItem.contents}} </div>
-                <div class="post-time"> {{ postItem.createdAt}} </div>
-            </li>
-        </ul>
+        <li>
+            <div class="post-title"> {{ postItem.title}} </div>
+            <div class="post-contents"> {{ postItem.contents}} </div>
+            <div class="post-time"> {{ postItem.createdAt}} </div>
+        </li>
     </div>
 </template>
 
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,11 +2,14 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <PostListItem
-        v-for="postItem in postItems"
-        :key="postItem._id"
-        :postItem="postItem"></PostListItem>
-      
+
+      <LoadingSpinner v-if="isLoading" />
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"></PostListItem>
+      </ul>
     </div>
   </div>
 </template>
@@ -14,19 +17,24 @@
 <script>
 import { fetchPosts } from '@/api/index';
 import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 
 export default {
   data() {
     return {
       postItems: [],
+      isLoading: false,
     }
   },
   components: {
-    PostListItem
+    PostListItem,
+    LoadingSpinner,
   },
   methods: {
     async fetchNotes() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
+      this.isLoading = false;
       this.postItems = data.posts;
     }
   },
