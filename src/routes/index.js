24e7686bb2776a1e12e6f6451cf3723f7c56e605
hjha <hjhaaa.github@gmail.com>
2022-11-15 21:26:58 +0900

    feat: vue 라우터 네비게이션 가드 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
 	mode: 'history',
 	routes: [
 		{
@@ -21,13 +22,16 @@ export default new VueRouter({
 		{
 			path: '/main',
 			component: () => import('@/views/MainPage.vue'),
+			meta: { auth: true },
 		},
 		{
 			path: '/add',
 			component: () => import('@/views/PostAddPage.vue'),
+			meta: { auth: true },
 		}, {
 			path: '/post/:id',
 			component: () => import('@/views/PostEditPage.vue'),
+			meta: { auth: true },
 
 		}, {
 			path: '*', // 정해진 path 이외의 페이지 라우팅, https://router.vuejs.org/guide/essentials/history-mode.html#html5-mode
@@ -35,3 +39,14 @@ export default new VueRouter({
 		},
 	],
 });
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.auth && !store.getters.isLogin) {
+		console.log('인증이 필요합니다.');
+		next('/login');
+	} else {
+		next();
+	}
+});
+
+export default router;
